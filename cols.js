6fc83a66186d9aa3687fedce5d876d66b4b919e3
Christopher <christopher13e11@gmail.com>
2018-11-27 14:53:17 -0600

    deleted commends

--- cols.js
+++ cols.js
@@ -4,8 +4,7 @@ class lockedCols extends deepFind{
         let mainTrack = 0 
         while(mainTrack < 9){ 
         let curHead = curTarg;
-        let allNums = [1,2,3,4,5,6,7,8,9];
-        //this.colorCell(curHead.cell, "blue");
+        let allNums = [1,2,3,4,5,6,7,8,9]; 
          
         for(let i=0; i<this.sectionMap.length; i++){ 
             if(!Array.isArray(curHead.value)){  
@@ -19,35 +18,12 @@ class lockedCols extends deepFind{
             
             let checkNum = allNums[0];
             let foundCount = [0,0,0];
-            curHead = curTarg;
-            /*
-            for(let cols=0; cols<3; cols++){
-                let colHead = curHead; 
-                for(let i=0; i<3; i++){  
-                    if(Array.isArray(colHead.value)){ 
-                        let findNum = colHead.value.filter(num => checkNum === parseInt(num));
-                        if(findNum > 0){  
-                            foundCount[cols]++; 
-                         } 
-                    } 
-                    colHead = colHead.down;
-                }
-                curHead = curHead.right;
-            } */
-            //if(foundCount < 2){ 
-                // DELETE ALL COLUMNS HERE
-                // DELETE ALL COLUMNS HERE
-                // DELETE ALL COLUMNS HERE
-              //  console.log("SOMETHING FOUND " + checkNum + " in " + curTarg.cell);  
-               //break;
-            //}
-            
+            curHead = curTarg; 
             foundCount = [0, 0, 0]; 
             curHead = curTarg;
             for( let rows = 0; rows < 3; rows++){
                 let rowHead = curHead;
-                for( let i=0; i<3; i++){
-                    //this.colorCell(rowHead.cell, "red");
+                for( let i=0; i<3; i++){ 
                     if(Array.isArray(rowHead.value)){ 
                         let findNum = rowHead.value.filter(num => checkNum === parseInt(num));
                         if(findNum > 0){  
@@ -57,16 +33,12 @@ class lockedCols extends deepFind{
                     rowHead = rowHead.right;
                 }
                 curHead = curHead.down;
-            } 
-            //Removes first number in array
+            }  
             allNums.shift(); 
-
-            //temp - avoids forever loop
+ 
             limit--;
             if(limit === 0){ break; }
-        } 
-        
- 
+        }  
         //Moves curTarg
         if(mainTrack === 2 || mainTrack === 5){
             curTarg = curTarg.down.down.down;
